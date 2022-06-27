a723a9372965bac1cda0020bdc1c1d4b2c9633d1
daeboJang <devjang2021@gmail.com>
2022-06-27 17:01:58 +0900

    Board 엔티티를 Builder 와 전체인자로 생성만 가능하도록 수정

--- src/main/java/me/kwanghee/board/model/Board.java
+++ src/main/java/me/kwanghee/board/model/Board.java
@@ -11,22 +11,27 @@ import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "board")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor
 @Builder
 public class Board {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	private String title;
-	private String content;
-	private String writer;
-	private Integer readCount;
+	private final String title;
+	private final String content;
+	private final String writer;
+	@Builder.Default private Integer readCount = 0;
 	
 	@DateTimeFormat(pattern = "YYYY-mm-dd HH:ii:ss")
 	private LocalDateTime createdAt;
@@ -36,8 +41,11 @@ public class Board {
 	
 	@PrePersist
 	public void setupDateTime() {
-		this.readCount = 0;
+		// final 필드는 생성자 에서만 초기화 할 수 있음
+		//this.readCount = 0;
 		this.createdAt = LocalDateTime.now();
 		this.updatedAt = this.createdAt;
 	}
 }
+
+
