2226908e3c37e7fb0107b799c7c92ddb2f962dba
daeboJang <devjang2021@gmail.com>
2022-06-29 16:40:41 +0900

    초기 게시판 데이터 주입
    게시글 50개로 변경
    작성자를 5명 중에서 임의로 넣도록 수정

--- src/main/java/me/kwanghee/Demo01Application.java
+++ src/main/java/me/kwanghee/Demo01Application.java
@@ -1,5 +1,7 @@
 package me.kwanghee;
 
+import java.util.Random;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,19 +23,16 @@ public class Demo01Application {
 			
 			@Override
 			public void run(String... args) throws Exception {
-				// 제목, 내용, 작성자
-//				repo.save(new Board("제목1", "게시글 내용임...1", "홍길동", 0));
-//				repo.save(new Board("제목2", "게시글 내용임...2", "홍길동", 0));
-//				repo.save(new Board("제목3", "게시글 내용임...3", "홍길순", 0));
-//				repo.save(new Board("제목4", "게시글 내용임...4", "홍길동", 0));
-//				repo.save(new Board("제목5", "게시글 내용임...5", "김길만", 0));
+				String[] people = {"홍길동", "김삿갓", "BTS", "임영웅", "아이유"};
+				
+				Random rand = new Random();
 				
 				// builder 사용해서 초기 데이터 삽입
-				for (int i=1; i <= 100; i++)
+				for (int i=1; i <= 50; i++)
 				{
 				repo.save(Board.builder().title("제목" + i)
 						.content("게시글 내용임..." + i)
-						.writer("홍길동")
+						.writer(people[rand.nextInt(5)])
 						.build());
 				} // for()
 			}
